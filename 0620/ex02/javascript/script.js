0a82f58c16193d9e6ddfc6341b1cd8b780ef9ca4
Toriikk <dotorii0216@naver.com>
2022-06-21 20:19:25 +0900

    0621ex02완성

--- 0620/ex02/javascript/script.js
+++ 0620/ex02/javascript/script.js
@@ -27,7 +27,7 @@ function start(){
    setInterval(function(){slide();},2000);
 }
 function slide(){
-   now = now==imgs?0:now+=1;
+   now = now==imgs?0:now+=1; /* 무한루틴 */
    $(".imgs>img").eq(now-1).css({"margin-top":"-800px"});   
    $(".imgs>img").eq(now).css({"margin-top":"0px"});
 }
